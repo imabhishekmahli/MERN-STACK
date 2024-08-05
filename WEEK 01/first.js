1f0534e478035d100d39b84806d2cbc94ab32bfd
imabhishekmahli <abhi8h3k@gmail.com>
2024-08-05 14:58:20 +0530

    used datatype string,number and boolean

--- WEEK 01/first.js
+++ WEEK 01/first.js
@@ -6,6 +6,19 @@
 // let a = 'abhishek';
 // console.log(a)
 
-const b = 87;
-b = 'rohan';
-console.log(b)
+// const b = 87;
+// b = 'rohan';
+// console.log(b)
+
+
+// --------------------------
+
+// datatype -> String
+//          -> Number
+//          -> boolean
+
+let firstName = 'abhishek'
+let age = 25
+let isMarried = false
+
+console.log('this person nanme is ' + firstName +' and his age is ' + age )
