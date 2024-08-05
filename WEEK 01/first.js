28cef0e9b707d4511abc204261f4c5ab09d037a9
imabhishekmahli <abhi8h3k@gmail.com>
2024-08-05 15:16:27 +0530

    print n number using for loop

--- WEEK 01/first.js
+++ WEEK 01/first.js
@@ -17,19 +17,31 @@
 //          -> Number
 //          -> boolean
 
-let firstName = 'abhishek'
-let age = 25
-let isMarried = true
+// let firstName = 'abhishek'
+// let age = 25
+// let isMarried = true
 
 // console.log('this person nanme is ' + firstName +' and his age is ' + age )
 
 // ---------------- if else statement 
 
-if(isMarried==true)
-{
-  console.log(firstName + 'is Married')
-}
+// if(isMarried==true)
+// {
+//   console.log(firstName + 'is Married')
+// }
 
-else{
-  console.log(firstName + 'is not married')
+// else{
+//   console.log(firstName + 'is not married')
+// }
+
+
+// --------------------------------
+
+// for loop 
+
+let a = 1;
+for(i=0;i<10;i++)
+{
+  console.log(a)
+  a=a+1;
 }
